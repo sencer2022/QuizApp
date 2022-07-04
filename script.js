c9ffdf539536256a417171ea0f9b975249afe0e0
sencer2022 <senceralpdogdu@gmail.com>
2022-07-04 03:40:06 +0300

    Added display of quesitons.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ Soru.prototype.cevabiKontrolEt = function(cevap){
 }
 
 let sorular = [
-    new Soru("1-Hangisi Javascript paket yönetim uygulamasıdır?", {a: "NodeJS", b: "Typscript", c: "Npm"}, "c"),
+    new Soru("1-Hangisi Javascript paket yönetim uygulamasıdır?", {a: "NodeJS", b: "Typscript", c: "Npm", d: "Nuget"}, "c"),
     new Soru("2-Hangisi Javascript paket yönetim uygulamasıdır?", {a: "NodeJS", b: "Typscript", c: "Npm"}, "c"),
     new Soru("3-Hangisi Javascript paket yönetim uygulamasıdır?", {a: "NodeJS", b: "Typscript", c: "Npm"}, "c"),
     new Soru("4-Hangisi Javascript paket yönetim uygulamasıdır?", {a: "NodeJS", b: "Typscript", c: "Npm"}, "c"),
@@ -28,11 +28,34 @@ const quiz = new Quiz(sorular);
 
 
 document.querySelector(".btn-start").addEventListener("click", function(){
-    if (quiz.sorular.length != quiz.soruIndex){
-        document.querySelector('.quiz_box').classList.add("active");
-        console.log(quiz.soruGetir());
+    document.querySelector('.quiz_box').classList.add("active");
+    soruGoster(quiz.soruGetir());
+});
+
+document.querySelector(".next_btn").addEventListener("click", function(){
+    if (quiz.sorular.length != quiz.soruIndex+1){
         quiz.soruIndex += 1;
+        soruGoster(quiz.soruGetir());
     }else{
-        console.log("Quiz bitti!")
+        console.log("Quiz bitti")
     }
 });
+
+function soruGoster(soru){
+    let question = `<span>${soru.soruMetni}</span>`;
+    let options = '';
+
+    for (let cevap in soru.cevapSecenekleri){
+        options += 
+            `
+                <div class="option">
+                    <span><b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]}</span>
+                </div>
+
+            `;
+    }
+
+    document.querySelector(".question-text").innerHTML = question;
+    document.querySelector(".option-list").innerHTML = options;
+
+}
